6f8eb797a934718ea7a0349c867681ceb5d453a0
Rachelle Milne <rachelle.fernandez@gmail.com>
2012-12-07 11:42:57 -0500

    Uses Serial.available; doesn't work

--- arduino_pong_serial.ino
+++ arduino_pong_serial.ino
@@ -5,64 +5,74 @@ Pong Controllers
  2 December 2012
  
  Modified Code from:
- Bruce Allen
- Date: 23/07/09
+ Making Things Talk
+ by Tom Igoe
  
  */
 
+int inByte = 0;
 
-int leftPin = 3;
-//int rightPin = A1;
-int btnPin = 7;
+//int leftPin = 3;
+int rightPin = A1;
+int btnPin = 13;
+int rightVal = 0;
+int btnVal = 1;
 
-//int leftVal;
-//int rightVal;
-int btnVal;
-
-int previousTime;
-int currentTime;
-int delayTime = 0;
-
+/*
 const int numVals = 3;
-
-long tempVals[numVals];
-
-long leftVal, pulse, inches;
+ long tempVals[numVals];
+ long leftVal, pulse, inches;
+ */
 
 void setup() {
   Serial.begin(9600);
-  pinMode(leftPin, INPUT);
-  //pinMode(rightPin, INPUT);
+  pinMode(rightPin, INPUT);
   pinMode(btnPin, INPUT_PULLUP);
+ 
+   while (Serial.available() <= 0) 
+   {
+   Serial.write('\r');
+   delay(300);
+   } 
 
-  previousTime = millis();
+
 }
 
 void loop() {
 
-  currentTime = millis();
+  if (Serial.available() > 0) {
 
-  if (currentTime - previousTime >= delayTime) 
-  {  
+    inByte = Serial.read();
+    Serial.print("I received: ");
+    Serial.println(inByte, DEC);
 
-    
-    pulse = pulseIn(leftPin, HIGH);
-    
-    inches = pulse/147;
-    leftVal = inches;
-
+    rightVal = analogRead(rightPin);
     btnVal = digitalRead(btnPin);
 
-    Serial.print(leftVal);
-
-    //  Serial.print(",");
-    //  Serial.print(rightVal);
-
+    Serial.print(rightVal);
     Serial.print(",");
     Serial.println(btnVal);
 
-    previousTime = millis(); 
-
   }
-
 }
+
+/*
+    pulse = pulseIn(leftPin, HIGH);
+ 
+ inches = pulse/147;
+ leftVal = inches;
+ 
+ btnVal = digitalRead(btnPin);
+ 
+ Serial.print(leftVal);
+ 
+ //  Serial.print(",");
+ //  Serial.print(rightVal);
+ 
+ Serial.print(",");
+ Serial.println(btnVal);
+ */
+
+
+
+
